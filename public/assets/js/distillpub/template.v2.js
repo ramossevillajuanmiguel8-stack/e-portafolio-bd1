603f37ee8ad93db08d2d30784c76f25e7ec46d76
miguel_ramos <ramossevillajuanmiguel8@gmail.com>
2026-08-10 23:55:21 -0500

    fix: aplicar formato con prettier y eslint

--- public/assets/js/distillpub/template.v2.js
+++ public/assets/js/distillpub/template.v2.js
@@ -2647,7 +2647,7 @@ d-citation-list .references .title {
 
           _.hooks.run('before-all-elements-highlight', env);
 
-          for (var i = 0, element; (element = env.elements[i++]); ) {
+          for (var i = 0, element; (element = env.elements[i++]);) {
             _.highlightElement(element, async === true, env.callback);
           }
         },
@@ -2771,7 +2771,7 @@ d-citation-list .references .title {
               return;
             }
 
-            for (var i = 0, callback; (callback = callbacks[i++]); ) {
+            for (var i = 0, callback; (callback = callbacks[i++]);) {
               callback(env);
             }
           },
@@ -8367,8 +8367,8 @@ p small {
   }
 
   function selection_order() {
-    for (var groups = this._groups, j = -1, m = groups.length; ++j < m; ) {
-      for (var group = groups[j], i = group.length - 1, next = group[i], node; --i >= 0; ) {
+    for (var groups = this._groups, j = -1, m = groups.length; ++j < m;) {
+      for (var group = groups[j], i = group.length - 1, next = group[i], node; --i >= 0;) {
         if ((node = group[i])) {
           if (next && node.compareDocumentPosition(next) ^ 4)
             next.parentNode.insertBefore(node, next);
